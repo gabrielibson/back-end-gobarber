c32c83298c6a8ac131d38801202efa6e59b80434
Gabriel Ibson <gabriel.souza@liferay.com>
2020-05-29 17:30:00 -0300

    create models, services, repositories and routes

---
+++ src/models/Users.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  udpatedAt: Date;
+}
+
+export default User;
